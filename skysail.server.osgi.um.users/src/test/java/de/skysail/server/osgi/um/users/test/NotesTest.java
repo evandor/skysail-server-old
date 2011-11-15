38eddf30b89e3fac8d540a5e43593132462569ba
Carsten Gräf <evandor@gmail.com>
2011-11-15 21:53:30 +0100

    wip

--- skysail.server.osgi.um.users/src/test/java/de/skysail/server/osgi/um/users/test/NotesTest.java
+++ skysail.server.osgi.um.users/src/test/java/de/skysail/server/osgi/um/users/test/NotesTest.java
@@ -1,33 +1,33 @@
-/**
- * 
- */
-package de.skysail.server.osgi.um.users.test;
-
-import junit.framework.TestCase;
-import de.skysail.server.osgi.um.users.User;
-
-/**
- * @author carsten
- *
- */
-public class NotesTest extends TestCase {
-
-	/**
-	 * Test method for {@link de.twenty11.skysail.server.osgi.um.users.User#getnote()}.
-	 */
-	public void testGetnote() {
-		User note = new User();
-//		note.setNote("test");
-//		assertEquals("test",note.getNote());
-	}
-
-	/**
-	 * Test method for {@link de.twenty11.skysail.server.osgi.um.users.User#getId()}.
-	 */
-	public void testGetId() {
-		User note = new User();
-		note.setId(new Long(17));
-		assertTrue(new Long(17).equals(note.getId()));
-	}
-
-}
+///**
+// * 
+// */
+//package de.skysail.server.osgi.um.users.test;
+//
+//import junit.framework.TestCase;
+//import de.skysail.server.osgi.um.users.User;
+//
+///**
+// * @author carsten
+// *
+// */
+//public class NotesTest extends TestCase {
+//
+//	/**
+//	 * Test method for {@link de.twenty11.skysail.server.osgi.um.users.User#getnote()}.
+//	 */
+//	public void testGetnote() {
+//		User note = new User();
+////		note.setNote("test");
+////		assertEquals("test",note.getNote());
+//	}
+//
+//	/**
+//	 * Test method for {@link de.twenty11.skysail.server.osgi.um.users.User#getId()}.
+//	 */
+//	public void testGetId() {
+//		User note = new User();
+//		note.setId(new Long(17));
+//		assertTrue(new Long(17).equals(note.getId()));
+//	}
+//
+//}
--- skysail.server.osgi.um.users/src/test/java/de/skysail/server/osgi/um/users/test/TestJunit4.java
+++ skysail.server.osgi.um.users/src/test/java/de/skysail/server/osgi/um/users/test/TestJunit4.java
@@ -1,37 +1,37 @@
-package de.skysail.server.osgi.um.users.test;
-
-import static org.junit.Assert.*;
-
-import java.util.List;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
-import de.skysail.server.osgi.um.users.User;
-import de.skysail.server.osgi.um.users.internal.UserDao;
-
-public class TestJunit4 {
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
-	@Test
-	public void testList() {
-		UserDao dao = new UserDao();
-		try {
-			dao.setSessionFactory(null);
-			List<User> list = dao.list();
-		}
-		catch (Exception e) {
-			
-		}
-		assertTrue(true);
-	}
-
-}
+//package de.skysail.server.osgi.um.users.test;
+//
+//import static org.junit.Assert.*;
+//
+//import java.util.List;
+//
+//import org.junit.After;
+//import org.junit.Before;
+//import org.junit.Test;
+//
+//import de.skysail.server.osgi.um.users.User;
+//import de.skysail.server.osgi.um.users.internal.UserDao;
+//
+//public class TestJunit4 {
+//
+//	@Before
+//	public void setUp() throws Exception {
+//	}
+//
+//	@After
+//	public void tearDown() throws Exception {
+//	}
+//
+//	@Test
+//	public void testList() {
+//		UserDao dao = new UserDao();
+//		try {
+//			dao.setSessionFactory(null);
+//			List<User> list = dao.list();
+//		}
+//		catch (Exception e) {
+//			
+//		}
+//		assertTrue(true);
+//	}
+//
+//}
