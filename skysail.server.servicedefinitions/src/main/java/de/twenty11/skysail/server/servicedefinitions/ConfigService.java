2f03e866c057c5bb23477c8f0ee9e2d54d3a9214
Carsten Gräf <evandor@gmail.com>
2012-02-06 10:55:45 +0100

    initial

---
+++ skysail.server.servicedefinitions/src/main/java/de/twenty11/skysail/server/servicedefinitions/ConfigService.java
@@ -0,0 +1,39 @@
+/**
+ *  Copyright 2011 Carsten Gräf
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * 
+ */
+package de.twenty11.skysail.server.servicedefinitions;
+
+import java.util.Properties;
+
+/**
+ * Defines a configuration service which will return values (strings, properties, ...) for a given
+ * identifier.
+ * 
+ * @author carsten
+ *
+ */
+public interface ConfigService {
+
+    /**
+     * looks for identifier and returns the value.
+     * @param identifier
+     * @return
+     */
+    public String getString(String identifier);
+    
+    public Properties getProperties(String identifier, boolean removePrefix);
+    
+}
---
+++ skysail.server.servicedefinitions/src/main/java/de/twenty11/skysail/server/servicedefinitions/UrlMapper.java
@@ -0,0 +1,47 @@
+/**
+ *  Copyright 2011 Carsten Gräf
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * 
+ */
+package de.twenty11.skysail.server.servicedefinitions;
+
+import java.util.Map;
+
+
+/**
+ * Implementors provide a <String,String> mapping via 'getUrlMapping' meant to describe a path -> classname relation.
+ * When a request for a specific path is received, the associated class is meant to "take care" of the request 
+ *
+ * @author carsten
+ *
+ */
+public interface UrlMapper {
+
+    /**
+     * The implementor provides a list of url mappings between paths and classes to be executed
+     * when such a path is being called.
+     * 
+     * The first String is a url restlet url mapping like 'notes/{command}' or the like,
+     * the second corresponds to a class name.
+     * 
+     * Ordering might be important, in that case make sure to provide for example a
+     * LinkedHashMap like this:
+     * 
+     * Set s = Collections.synchronizedMap(new LinkedHashMap(...))
+     *
+     * @return a map of path / classname pairs
+     */
+    Map<String, String> getUrlMapping();
+
+}
