9db4a7f921777d0d83f2b03ada2cb42fa8adc9b3
graefca <carsten.graef@kvb.de>
2012-07-09 17:37:27 +0200

    wip

---
+++ skysail.server/src/test/java/de/twenty11/skysail/server/integrationtest/SkysailServerOsgiTest.java
@@ -0,0 +1,67 @@
+package de.twenty11.skysail.server.integrationtest;
+
+import static org.junit.Assert.assertTrue;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.provision;
+import static org.ops4j.pax.tinybundles.core.TinyBundles.bundle;
+
+import java.io.InputStream;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.ExamReactorStrategy;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.ops4j.pax.exam.spi.reactors.AllConfinedStagedReactorFactory;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+
+import de.twenty11.skysail.common.test.SkysailCommonOsgiTest;
+import de.twenty11.skysail.server.internal.Activator;
+
+@RunWith(JUnit4TestRunner.class)
+@ExamReactorStrategy(AllConfinedStagedReactorFactory.class)
+public class SkysailServerOsgiTest extends SkysailCommonOsgiTest {
+
+    @Inject
+    private BundleContext context;
+
+    @Override
+	public List<Option> getOptions() {
+        List<Option> options = super.getOptions();
+        options.add(mavenBundle("de.twentyeleven.skysail","skysail.common", "0.3.2-SNAPSHOT"));
+        return options;
+	}
+
+    @Configuration
+    public Option[] config() {
+        List<Option> options = getOptions();
+        
+        InputStream bundleUnderTest = bundle().add(Activator.class)
+                .set(Constants.BUNDLE_SYMBOLICNAME, "skysail.server")
+                .set(Constants.IMPORT_PACKAGE, "de.twenty11.skysail.common.test")
+                .build();
+
+        options.add(provision(bundleUnderTest));
+        return options.toArray(new Option[getOptions().size()]);
+    }
+
+    @Test
+    public void shouldFindCommonBundleInActiveState() {
+        Bundle skysailCommonBundle = null;
+        Bundle[] bundles = context.getBundles();
+        for (Bundle bundle : bundles) {
+            if (bundle.getSymbolicName().equals("skysail.common")) {
+                skysailCommonBundle = bundle;
+            }
+        }
+        assertTrue(skysailCommonBundle != null);
+        assertTrue(skysailCommonBundle.getState() == 32);
+    }
+
+}
