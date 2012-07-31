fea0577c8b10978b0a1a2ff25d305bcf691441a7
graefca <carsten.graef@kvb.de>
2012-07-31 18:16:37 +0200

    wip

--- skysail.server/src/test/java/de/twenty11/skysail/server/integrationtest/SkysailServerOsgiSetup.java
+++ skysail.server/src/test/java/de/twenty11/skysail/server/integrationtest/SkysailServerOsgiSetup.java
@@ -2,14 +2,16 @@ package de.twenty11.skysail.server.integrationtest;
 
 import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
 
+import java.util.EnumSet;
 import java.util.List;
 
 import org.ops4j.pax.exam.Option;
 
+import de.twenty11.skysail.common.osgi.PaxExamOptionSet;
 import de.twenty11.skysail.common.osgi.SkysailCommonOsgiSetup;
 
 /**
- * This class defines the bundles skysail.common depends on (used by pax-exam), i.e.
+ * This class defines the bundles skysail.server depends on (used by pax-exam), i.e.
  * the bundles to be used containing the non-optional packages imported by this bundle.
  * 
  * The provided List with options does not contain the current bundle itself!
@@ -20,8 +22,9 @@ import de.twenty11.skysail.common.osgi.SkysailCommonOsgiSetup;
 public class SkysailServerOsgiSetup extends SkysailCommonOsgiSetup {
 
     @Override
-	public List<Option> getOptions() {
-        List<Option> options = super.getOptions();
+	public List<Option> getOptions(EnumSet<PaxExamOptionSet> optionSets) {
+        List<Option> options = super.getOptions(optionSets);
+        
         options.add(mavenBundle("de.twentyeleven.skysail","skysail.common", "0.3.2-SNAPSHOT"));
 
         // restlet
