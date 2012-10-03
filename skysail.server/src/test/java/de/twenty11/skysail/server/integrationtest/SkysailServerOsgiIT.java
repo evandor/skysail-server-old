a65011d1fe8d038eadfb1eacb884f35f77086a18
Carsten Gräf <evandor@gmail.com>
2012-10-03 21:39:18 +0200

    wip

--- skysail.server/src/test/java/de/twenty11/skysail/server/integrationtest/SkysailServerOsgiIT.java
+++ skysail.server/src/test/java/de/twenty11/skysail/server/integrationtest/SkysailServerOsgiIT.java
@@ -8,9 +8,7 @@ import java.util.EnumSet;
 import java.util.List;
 
 import javax.inject.Inject;
-import javax.persistence.EntityManager;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Option;
@@ -20,13 +18,11 @@ import org.ops4j.pax.exam.junit.JUnit4TestRunner;
 import org.ops4j.pax.exam.spi.reactors.AllConfinedStagedReactorFactory;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.framework.ServiceReference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import de.twenty11.skysail.common.osgi.PaxExamOptionSet;
-import de.twenty11.skysail.server.SkysailEntityManagerProvider;
 import de.twenty11.skysail.server.services.EntityManagerProvider;
 
 @RunWith(JUnit4TestRunner.class)
@@ -35,28 +31,27 @@ public class SkysailServerOsgiIT {
 
     private static Logger logger = LoggerFactory.getLogger(SkysailServerOsgiIT.class.getName());
 
-	private List<PaxExamOptionSet> dependencies = new ArrayList<PaxExamOptionSet>();
+    private List<PaxExamOptionSet> dependencies = new ArrayList<PaxExamOptionSet>();
 
     @Inject
     private BundleContext context;
 
     @Configuration
     public Option[] config() {
-    	
-		dependencies.add(PaxExamOptionSet.BASE);
-		dependencies.add(PaxExamOptionSet.DEBUGGING);
+
+        dependencies.add(PaxExamOptionSet.BASE);
+        dependencies.add(PaxExamOptionSet.DEBUGGING);
 
         SkysailServerOsgiSetup setup = new SkysailServerOsgiSetup();
         List<Option> options = setup.getOptions(EnumSet.copyOf(dependencies));
-        
+
         // _this_ bundle from target directory
-        String currentBundleSource = "file:target/skysail.server-"+setup.getProjectVersion()+".jar";
+        String currentBundleSource = "file:target/skysail.server-" + setup.getProjectVersion() + ".jar";
         logger.info("adding {} to tests...", currentBundleSource);
         options.add(bundle(currentBundleSource));
-        
+
         return options.toArray(new Option[options.size()]);
     }
-
 
     @Test
     public void shouldFindCommonBundleInActiveState() {
@@ -69,20 +64,22 @@ public class SkysailServerOsgiIT {
     @Test
     public void shouldFindSkysailDatasourceService() {
         Bundle bundle = getBundleForSymbolicName("skysail.server");
-        ServiceReference skysailDatasourceReference = context.getServiceReference("de.twenty11.skysail.server.services.DataSourceProvider");
+        ServiceReference skysailDatasourceReference = context
+                .getServiceReference("de.twenty11.skysail.server.services.DataSourceProvider");
         assertTrue(skysailDatasourceReference != null);
     }
 
     @Test
     public void shouldFindSkysailEntityManagerProvider() {
         Bundle bundle = getBundleForSymbolicName("skysail.server");
-        ServiceReference skysailDatasourceReference = context.getServiceReference("de.twenty11.skysail.server.SkysailEntityManagerProvider");
+        ServiceReference skysailDatasourceReference = context
+                .getServiceReference("de.twenty11.skysail.server.SkysailEntityManagerProvider");
         assertTrue(skysailDatasourceReference != null);
-        EntityManagerProvider service = (EntityManagerProvider)context.getService(skysailDatasourceReference);
+        EntityManagerProvider service = (EntityManagerProvider) context.getService(skysailDatasourceReference);
         assertTrue(service != null);
-        //EntityManager entityManager = service.getEntityManager("SkysailPU");
+        // EntityManager entityManager = service.getEntityManager("SkysailPU");
     }
-    
+
     private Bundle getBundleForSymbolicName(String symbolicName) {
         Bundle myBundle = null;
         Bundle[] bundles = context.getBundles();
