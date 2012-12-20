0a5aa1a345c831b0d47f697458a2be8bdcb59d98
graefca <carsten.graef@kvb.de>
2012-12-20 17:47:31 +0100

    wip

---
+++ skysail.server/src/test/java/de/twenty11/skysail/server/test/InputReportIncident.java
@@ -0,0 +1,16 @@
+package de.twenty11.skysail.server.test;
+
+public class InputReportIncident {
+
+	private String id;
+	private String name;
+
+	public void setIncidentId(String string) {
+		this.id = string;
+	}
+
+	public void setName(String string) {
+		this.name = string;
+	}
+
+}
