544b540128dd8064d796df79a26cb2c9b6f96b52
evandor <evandor@gmail.com>
2013-01-24 08:20:22 +0100

    wip

--- skysail.server/src/main/java/de/twenty11/skysail/server/config/Configuration.java
+++ skysail.server/src/main/java/de/twenty11/skysail/server/config/Configuration.java
@@ -1,24 +1,31 @@
 package de.twenty11.skysail.server.config;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Dictionary;
-
-import javax.sql.DataSource;
+import java.util.List;
 
 import org.apache.commons.dbcp.BasicDataSource;
 import org.osgi.service.cm.ConfigurationException;
-import org.osgi.service.cm.ManagedService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import de.twenty11.skysail.common.config.ConfigurationProvider;
 import de.twenty11.skysail.server.Constants;
-import de.twenty11.skysail.server.services.DataSourceProvider;
 
-public class Configuration implements ManagedService {
+public class Configuration implements ConfigurationProvider {// used to implements ManagedService,
 
     private final static Logger logger = LoggerFactory.getLogger(Configuration.class);
     private static BasicDataSource defaultDS;
 
-    @Override
+    private List<ConfigurationProvider> configurationProviders = Collections
+            .synchronizedList(new ArrayList<ConfigurationProvider>());
+
+    public Configuration() {
+        System.out.println("Hier");
+    }
+
+    // @Override
     public synchronized void updated(Dictionary properties) throws ConfigurationException {
         if (properties != null) {
             // TODO set in memory defaults
@@ -31,7 +38,7 @@ public class Configuration implements ManagedService {
                 newUsername = (String) properties.get(Constants.SKYSAIL_DB_USERNAME);
                 newPassword = (String) properties.get(Constants.SKYSAIL_DB_PASSWORD);
                 newDbUrl = (String) properties.get(Constants.SKYSAIL_DB_URL);
-                //port = Integer.parseInt((String) properties.get(Constants.SKYSAIL_DB_USERNAME));
+                // port = Integer.parseInt((String) properties.get(Constants.SKYSAIL_DB_USERNAME));
             } catch (Exception e) {
                 // Ignore, use defaults
                 logger.warn("could not set skysail database connection due to exception", e);
@@ -49,5 +56,24 @@ public class Configuration implements ManagedService {
         return defaultDS;
     }
 
+    public void addConfigurationProvider(ConfigurationProvider provider) {
+        if (!(provider instanceof Configuration)) {
+            configurationProviders.add(provider);
+        }
+    }
+
+    public void removeConfigurationProvider(ConfigurationProvider provider) {
+        configurationProviders.remove(provider);
+    }
+
+    @Override
+    public String getConfigForKey(String key) {
+        for (ConfigurationProvider provider : configurationProviders) {
+            if (provider.getConfigForKey(key) != null) {
+                return provider.getConfigForKey(key);
+            }
+        }
+        return null;
+    }
 
 }
