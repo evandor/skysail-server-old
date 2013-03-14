375c172f86a9ab9709eca5e4d97769fe1f0b3cbf
evandor <evandor@gmail.com>
2013-03-14 17:23:28 +0100

    wip

---
+++ skysail.server/src/main/java/de/twenty11/skysail/server/converter/ToCsvConverter.java
@@ -0,0 +1,135 @@
+package de.twenty11.skysail.server.converter;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map.Entry;
+
+import org.restlet.data.MediaType;
+import org.restlet.engine.converter.ConverterHelper;
+import org.restlet.engine.resource.VariantInfo;
+import org.restlet.ext.jackson.JacksonRepresentation;
+import org.restlet.representation.Representation;
+import org.restlet.representation.StringRepresentation;
+import org.restlet.representation.Variant;
+import org.restlet.resource.Resource;
+
+import de.twenty11.skysail.common.Presentable;
+import de.twenty11.skysail.common.responses.FailureResponse;
+import de.twenty11.skysail.common.responses.SkysailResponse;
+
+public class ToCsvConverter extends ConverterHelper {
+
+    private static final VariantInfo VARIANT_JSON = new VariantInfo(MediaType.APPLICATION_EXCEL);
+
+    @Override
+    public float score(Object source, Variant target, Resource resource) {
+        float result = -1.0F;
+        if (!(source instanceof de.twenty11.skysail.common.responses.SkysailResponse)) {
+            return 0.0F;
+        }
+        if (target.getMediaType().equals(MediaType.TEXT_CSV)) {
+            result = 1.0F;
+        } else {
+            result = 0.1F;
+        }
+        return result;
+    }
+
+    @Override
+    public <T> float score(Representation source, Class<T> target, Resource resource) {
+        float result = -1.0F;
+
+        if (source instanceof JacksonRepresentation<?>) {
+            result = 1.0F;
+        } else if ((target != null) && JacksonRepresentation.class.isAssignableFrom(target)) {
+            result = 1.0F;
+        } else if (VARIANT_JSON.isCompatible(source)) {
+            result = 0.8F;
+        }
+
+        return result;
+    }
+
+    @Override
+    public <T> T toObject(Representation source, Class<T> target, Resource resource) throws IOException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Class<?>> getObjectClasses(Variant source) {
+        List<Class<?>> result = null;
+
+        if (VARIANT_JSON.isCompatible(source)) {
+            result = addObjectClass(result, Object.class);
+            result = addObjectClass(result, JacksonRepresentation.class);
+        }
+
+        return result;
+    }
+
+    @Override
+    public List<VariantInfo> getVariants(Class<?> source) {
+        List<VariantInfo> result = null;
+        if (source != null) {
+            result = addVariant(result, VARIANT_JSON);
+        }
+        return result;
+    }
+
+    @Override
+    public Representation toRepresentation(Object source, Variant target, Resource resource) {
+        Representation representation;
+        try {
+            representation = new StringRepresentation(toCsv((SkysailResponse) source, resource));
+        } catch (Exception e) {
+            representation = new StringRepresentation(toCsv(new FailureResponse(e), resource));
+        }
+        representation.setMediaType(MediaType.TEXT_HTML);
+        return representation;
+    }
+
+    private String toCsv(SkysailResponse<List<?>> skysailResponse, Resource resource) {
+        if (skysailResponse instanceof FailureResponse) {
+            return skysailResponse.getMessage();
+        }
+
+        Object skysailResponseAsObject = skysailResponse.getData();
+        if (skysailResponseAsObject != null) {
+            return createCsvForContent(skysailResponseAsObject);
+        } else {
+            return "no data";
+        }
+    }
+
+    private String createCsvForContent(Object skysailResponseAsObject) {
+        StringBuilder sb = new StringBuilder("");
+        if (skysailResponseAsObject instanceof List) {
+            List<?> data = (List<?>) skysailResponseAsObject;
+            int i = 0;
+            if (data != null) {
+                for (Object object : data) {
+                    i = handleDataElementsForList(sb, i, object);
+                }
+            }
+        } else {
+            handleDataElementsForList(sb, 1, skysailResponseAsObject);
+        }
+        sb.append("");
+        return sb.toString();
+    }
+
+    private int handleDataElementsForList(StringBuilder sb, int i, Object object) {
+        if (object instanceof Presentable) {
+            Presentable presentable = (Presentable) object;
+            for (Entry<String, Object> row : presentable.getContent().entrySet()) {
+                sb.append("\"");
+                sb.append(row.getKey().replace("\"", "\\\""));
+                sb.append("\"");
+            }
+            sb.append("\n");
+        }
+        return i;
+    }
+
+}
---
+++ skysail.server/src/test/java/de/twenty11/skysail/server/converter/ToCsvConverterTest.java
@@ -0,0 +1,19 @@
+package de.twenty11.skysail.server.converter;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ToCsvConverterTest {
+
+    @Before
+    public void setUp() throws Exception {
+        new ToCsvConverter;
+    }
+    
+    @Test
+    public void aaa() {
+        
+    }
+
+}
