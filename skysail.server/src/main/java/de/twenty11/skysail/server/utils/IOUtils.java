e55b1f6852c924c3354fdf37c57231b952d25ed4
evandor <evandor@gmail.com>
2013-06-18 15:27:54 +0200

    fix

--- skysail.server/src/main/java/de/twenty11/skysail/server/utils/IOUtils.java
+++ skysail.server/src/main/java/de/twenty11/skysail/server/utils/IOUtils.java
@@ -4,14 +4,9 @@ package de.twenty11.skysail.server.utils;
 public class IOUtils {
 
     public static String convertStreamToString(java.io.InputStream is) {
-        // StringWriter writer = new StringWriter();
-        // try {
-        // org.apache.commons.io.IOUtils.copy(is, writer, "UTF-8");
-        // return writer.toString();
-        // } catch (IOException e) {
-        // throw new RuntimeException("problem reading input stream", e);
-        // }
-        //
+        if(is == null) {
+        	return null;
+        }
         java.util.Scanner s = new java.util.Scanner(is, "UTF-8").useDelimiter("\\A");
         return s.hasNext() ? s.next() : "";
     }
