4a791d2e274f98e89f29a64e9b5288f8514288cd
evandor <evandor@gmail.com>
2013-07-02 13:29:07 +0200

    wip

---
+++ skysail.server.security.shiro/src/main/java/de/twenty11/skysail/server/security/shiro/MenuApplication.java
@@ -0,0 +1,16 @@
+package de.twenty11.skysail.server.security.shiro;
+
+import de.twenty11.skysail.server.restlet.SkysailApplication;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MenuApplication extends SkysailApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(MenuApplication.class);
+
+    @Override
+    protected void attach() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
---
+++ skysail.server.security.shiro/src/test/java/test/de/twenty11/skysail/server/menu/domain/MenuTest.java
@@ -0,0 +1,18 @@
+package test.de.twenty11.skysail.server.menu.domain;
+
+import de.twenty11.skysail.server.security.shiro.domain.Menu;
+
+import org.junit.Before;
+
+public class MenuTest {
+
+    private Menu root;
+
+    private Menu mainMenu;
+
+    @Before
+    public void setUp() throws Exception {
+        root = new Menu();
+        mainMenu = new Menu(root, "name", "link");
+    }
+}
