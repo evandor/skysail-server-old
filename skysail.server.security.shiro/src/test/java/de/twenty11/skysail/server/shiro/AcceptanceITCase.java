37826a098ebd9ef875fc73f2dba64ccb221e7119
evandor <evandor@gmail.com>
2013-08-28 16:59:27 +0200

    wip

---
+++ skysail.server.security.shiro/src/test/java/de/twenty11/skysail/server/shiro/AcceptanceITCase.java
@@ -0,0 +1,9 @@
+package de.twenty11.skysail.server.shiro;
+
+import net.thucydides.jbehave.ThucydidesJUnitStories;
+
+public class AcceptanceITCase extends ThucydidesJUnitStories {
+    public AcceptanceITCase() {
+    	//findStoriesCalled("**/adding_folder/xss.story");
+    }
+}
---
+++ skysail.server.security.shiro/src/test/java/de/twenty11/skysail/server/shiro/AcceptanceTests.java
@@ -0,0 +1,30 @@
+package de.twenty11.skysail.server.shiro;
+
+import de.twenty11.skysail.server.ResourceTestWithUnguardedAppication;
+import de.twenty11.skysail.server.internal.DefaultSkysailApplication;
+import de.twenty11.skysail.server.internal.SkysailComponent;
+import org.jbehave.core.annotations.BeforeStory;
+import org.restlet.Component;
+import org.restlet.data.Protocol;
+
+public class AcceptanceTests extends ResourceTestWithUnguardedAppication<DefaultSkysailApplication> {
+
+    private static Component component = new SkysailComponent();
+
+    static {
+        component.getServers().add(Protocol.HTTP, TEST_PORT);
+        try {
+            component.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @BeforeStory
+    public void setUp() {
+        DefaultSkysailApplication application = (DefaultSkysailApplication) setUpApplication(new DefaultSkysailApplication(null));
+        //application.setEntityManager(getEmfForTests("NotesPU"));
+        component.getDefaultHost().attach(application);
+    }
+
+}
