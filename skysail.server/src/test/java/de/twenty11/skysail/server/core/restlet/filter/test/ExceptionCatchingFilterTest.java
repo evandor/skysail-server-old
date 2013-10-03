3162cbbb783076221a1e422cdab3bcd7ba9ecc5f
Carsten Gräf <evandor@gmail.com>
2013-10-03 09:24:26 +0200

    preparing release

--- skysail.server/src/test/java/de/twenty11/skysail/server/core/restlet/filter/test/ExceptionCatchingFilterTest.java
+++ skysail.server/src/test/java/de/twenty11/skysail/server/core/restlet/filter/test/ExceptionCatchingFilterTest.java
@@ -17,7 +17,6 @@ import de.twenty11.skysail.server.core.restlet.filter.ExceptionCatchingFilter;
 import de.twenty11.skysail.server.core.restlet.filter.FilterResult;
 import de.twenty11.skysail.server.core.restlet.testentities.SimpleEntity;
 import de.twenty11.skysail.server.core.restlet.testresources.MyEntityResource;
-import de.twenty11.unitprofile.annotations.Profile;
 
 public class ExceptionCatchingFilterTest {
 
@@ -38,7 +37,7 @@ public class ExceptionCatchingFilterTest {
     }
 
     @Test
-    @Profile
+    // @Profile
     public void delegates_to_next_filter_if_before_returns_continue() {
 
         Request request = Mockito.mock(Request.class);
